39f0aa0df0fd11410b11adbfd7fbac9d42a0f136
Leonardo Bork <leonardomerker@gmail.com>
2019-12-23 09:45:10 -0300

    add utils test

--- ses-example/tests/unit/test-handler.js
+++ ses-example/tests/unit/test-handler.js
@@ -1,22 +1,19 @@
 'use strict';
 
-const app = require('../../functions.js/index.js');
+// const app = require('../../functions.js/index.js');
 const chai = require('chai');
 const expect = chai.expect;
 var event, context;
 
-describe('Tests index', function () {
-    it('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+// describe('Tests index', function () {
+//     it('verifies successful response', async () => {
+//         const result = await app.lambdaHandler(event, context)
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+//         expect(result).to.be.an('object');
+//         expect(result.statusCode).to.equal(200);
+//         expect(result.body).to.be.an('string');
 
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
-    });
-});
+//         expect(response).to.be.an('object');
+//         // expect(response.location).to.be.an("string");
+//     });
+// });
---
+++ ses-example/tests/unit/utils/utils-test.js
@@ -0,0 +1,38 @@
+const emailUtils = require('../../../utils/email/email-utils')
+const chai = require('chai');
+const expect = chai.expect;
+
+const TEMPLATE_NAME = 'template-example';
+
+const CODE_VALUE = `TEST_${ Math.random() * 1000 }`;
+const CONTENT_OBJECT = [{ key: 'code', value: CODE_VALUE }];
+
+describe('Tests Email Utils', () => {
+  it('verifies if templates are being fetched', async () => {
+    const TEMPLATE_LENGTH = 15685;
+    const template = await emailUtils.getTemplateContent(TEMPLATE_NAME);
+    
+    expect(template).to.be.an('string');
+    expect(template.length).to.equal(TEMPLATE_LENGTH)
+  })
+
+  it('verifies if content is being binded in templates', async () => {
+    const MATCH_REGEX = new RegExp(CODE_VALUE, 'g');
+
+    const template = await emailUtils.getTemplateContent(TEMPLATE_NAME);
+
+    const bindedTemplate = emailUtils.bindContent(template, CONTENT_OBJECT);
+
+    expect(bindedTemplate).to.match(MATCH_REGEX);
+    expect(bindedTemplate).to.be.an('string');
+  })
+
+  it('verifies if template is being mounted correctly', async () => {
+    const MATCH_REGEX = new RegExp(CODE_VALUE, 'g');
+
+    const mountedTemplate = await emailUtils.mountTemplate(TEMPLATE_NAME, CONTENT_OBJECT);
+
+    expect(mountedTemplate).to.match(MATCH_REGEX);
+    expect(mountedTemplate).to.be.an('string');
+  })
+})
